b3083a1fafb8517127abaacd6ba05e8120855ff4
sugeeth <sugeethv1999@gmail.com>
2025-01-19 14:46:28 +0530

    removed author names extent

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,14 +8,12 @@ import pages.LoginPage;
 
 import static constants.TestGroupConstants.REGRESSION;
 import static constants.TestGroupConstants.SANITY;
-import static constants.CodeAuthors.HASHIRAMA;
-import static constants.CodeAuthors.TOBIRAMA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LoginTest extends BaseTest {
 
-    @Test(dataProvider = "singleLoginData", dataProviderClass = ExcelDataProvider.class,groups={SANITY,REGRESSION,HASHIRAMA})
+    @Test(dataProvider = "singleLoginData", dataProviderClass = ExcelDataProvider.class,groups={SANITY,REGRESSION})
     public void testValidLogin(String testcaseid, String password, String description, String username) {
         LoginPage loginPage = new LoginPage(getPage());
         loginPage.navigateToApp();
@@ -23,7 +21,7 @@ public class LoginTest extends BaseTest {
         assertThat(loginPage.getPageTitle()).as("Page Title not Matching").isEqualTo("Swag Labs");
     }
 
-    @Test(dataProvider = "singleLoginData", dataProviderClass = ExcelDataProvider.class, groups={REGRESSION,TOBIRAMA})
+    @Test(dataProvider = "singleLoginData", dataProviderClass = ExcelDataProvider.class, groups={REGRESSION})
     public void testValidLogin2(String testcaseid, String password, String description, String username) {
         LoginPage loginPage = new LoginPage(getPage());
         loginPage.navigateToApp();
--- src/test/java/tests/LoginTest2.java
+++ src/test/java/tests/LoginTest2.java
@@ -5,20 +5,19 @@ import dataproviders.ExcelDataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
-import static constants.CodeAuthors.MADARA;
+
 import static constants.TestGroupConstants.SMOKE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LoginTest2 extends BaseTest {
 
-
-    @Test(dataProvider = "multipleLoginData", dataProviderClass = ExcelDataProvider.class, groups={SMOKE,MADARA})
+    @Test(dataProvider = "multipleLoginData", dataProviderClass = ExcelDataProvider.class, groups={SMOKE})
     public void testValidLogin3(String testcaseid, String password, String description, String username) {
         LoginPage loginPage = new LoginPage(getPage());
         loginPage.navigateToApp();
         loginPage.login(username, password);
-        assertThat(loginPage.getPageTitle()).as("Page Title not Matching").isEqualTo("Swag Lab");
+        assertThat(loginPage.getPageTitle()).as("Page Title not Matching").isEqualTo("Swag Labs");
     }
 
 }
